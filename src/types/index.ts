5b48a6aa9ab67e7063299874b021c2865067371e
leonard.lind@hexagon.com-at-786635977109 <leonard.lind@hexagon.com>
2025-10-29 16:22:15 +0100

    clean up

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,11 @@
 export interface PendingImage {
   id: string;
   previewUrl: string;
-  saved: boolean; // ✅ should be a boolean
+  saved: boolean;
   species?: string;
   experiencePoint?: string;
   sensorId?: string;
   deploymentId?: string;
   experienceId?: string;
-  // ... any other fields you have
+
 }
